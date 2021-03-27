9e0aeee918c88eab5057bdcc047f85ec683c18b5
Nayanna Nara <nayanna501@gmail.com>
2021-03-27 20:04:31 -0300

    integraçao c/ github ok

--- vough_backend/api/integrations/github.py
+++ vough_backend/api/integrations/github.py
@@ -11,11 +11,38 @@ class GithubApi:
 
         :login: login da organização no Github
         """
-        return {}
+        #https://api.github.com/orgs/ministrycentered
+        resposta = requests.get(f'https://api.github.com/orgs/{login}')
+
+        resposta.json() if resposta.status_code == 200 else resposta.status_code
 
     def get_organization_public_members(self, login: str) -> int:
         """Retorna todos os membros públicos de uma organização
 
         :login: login da organização no Github
         """
-        return 0
+        resposta = requests.get(f'https://api.github.com/orgs/{login}/public_members')
+        status = resposta.status_code
+        qtd = len(resposta.json())
+
+        if status == 200:
+            qtd if qtd > 0 else 0
+        else:
+            return resposta.status_code
+        #https://api.github.com/orgs/ministrycentered/public_members
+        #https://api.github.com/orgs/ministrycentered/repos
+
+    def get_organization_public_repos(self, login: str) -> int:
+        """Retorna todos os respositorios públicos de uma organização
+
+        :login: login da organização no Github
+        """
+        resposta = requests.get(f'https://api.github.com/orgs/{login}/repos')
+
+        status = resposta.status_code
+        qtd = len(resposta.json())
+
+        if status == 200:
+            qtd if qtd > 0 else 0
+        else:
+            return resposta.status_code
